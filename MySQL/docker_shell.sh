6020253c33a31cc5b03ce3f5a38ab887dadf3b4b
unliar <370732889@qq.com>
2019-08-31 00:32:21 +0800

    feat: fix shell

--- MySQL/docker_shell.sh
+++ MySQL/docker_shell.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-docker run -p 3306:3306\ 
---name mymysql\
---restart=always\
--v $PWD/mysqldata:/var/lib/mysql\
--e MYSQL_ROOT_PASSWORD=pass\
--d mysql:5.6
+docker run -p 3306:3306 --name mymysql --restart=always -v $PWD/mysqldata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=pass -d mysql:latest
--- Redis/docker_shell.sh
+++ Redis/docker_shell.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-docker run --name myredis\
--p 6379:6379 -d\
---restart=always\
--v $PWD/redisdata:/data redis:latest redis-server --appendonly yes\
---requirepass "pass"
+docker run --name myredis -p 6379:6379 -d --restart=always -v $PWD/redisdata:/data redis:latest redis-server --appendonly yes --requirepass "pass"
